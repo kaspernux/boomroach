bb6d05a74459fc6156e846ce66117ef3ec737bf2
Kaspernux <ouangapou@gmail.com>
2025-05-30 23:36:39 +0000

    Initial commit – Boomroach Hydra Bot core modules

--- hydra_bot/telegram_bot/bot.py
+++
@@ -1,6 +0,0 @@
-from telegram.ext import ApplicationBuilder, CommandHandler
-from handlers.main import start
-from commands.trade import trade
-
-# Placeholder for backend integration
-from backend.services.trading_service import execute_trade  # Assume future implementation
